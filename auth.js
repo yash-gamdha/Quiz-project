9f452ed47aed200d60056690e3e66f5ac5bd17e6
yash-gamdha <yashgamdha97@gmail.com>
2025-03-16 18:14:38 +0530

    few changes here and there

--- auth.js
+++ auth.js
@@ -201,11 +201,9 @@ document.addEventListener("DOMContentLoaded", () => {
                     } else {
                         if (response && response.username) {
                             window.location.href = "main.html";
-                            // window.open("https://quiz-project-js.vercel.app/main.html", "_blank");
                         } else {
                             if (response && response.username) {
-                                window.location.href = "./main.html";
-                                // window.open("http://localhost:63342/Quiz-project/main.html", "_blank");
+                                window.location.href = "main.html";
                             } else {
                                 showError(document.getElementById("log_in_error"), "Login failed. Please try again.");
                             }
